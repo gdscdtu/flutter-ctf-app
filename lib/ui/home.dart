13b641b84dbda240a8876375a6411dd4567905af
TrungJamin <68997072+TrungJamin@users.noreply.github.com>
2022-04-10 17:09:04 +0700

    Complete level 7, 8

--- lib/ui/home.dart
+++ lib/ui/home.dart
@@ -8,6 +8,11 @@ import 'level2/level2.dart';
 import 'level3/level3.dart';
 import 'level4/level4.dart';
 import 'level5/level5.dart';
+import 'level6/level6.dart';
+import 'level7/level7.dart';
+import 'level8/level8.dart';
+import 'level9/level9.dart';
+import 'level10/level10.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -56,6 +61,26 @@ class HomeScreen extends StatelessWidget {
         );
       case 5:
         return Level5Screen(
+          level: level!,
+        );
+      case 6:
+        return Level6Screen(
+          level: level!,
+        );
+      case 7:
+        return Level7Screen(
+          level: level!,
+        );
+      case 8:
+        return Level8Screen(
+          level: level!,
+        );
+      case 9:
+        return Level9Screen(
+          level: level!,
+        );
+      case 10:
+        return Level10Screen(
           level: level!,
         );
       default:
---
+++ lib/ui/level7/level7.dart
@@ -0,0 +1,164 @@
+import 'package:confetti/confetti.dart';
+import "package:flutter/material.dart";
+import 'package:flutter/services.dart';
+import 'package:flutter_ctf_app/ui/background.dart';
+import 'dart:math';
+
+import '../../consts/my_colors.dart';
+import '../../consts/my_icons.dart';
+import '../../helper/on_submit.dart';
+
+class Level7Screen extends StatefulWidget {
+  const Level7Screen({Key? key, required this.level})
+      : assert(level == 7),
+        super(key: key);
+
+  final int level;
+  @override
+  State<Level7Screen> createState() => _Level7ScreenState();
+}
+
+class _Level7ScreenState extends State<Level7Screen> {
+  final TextEditingController _passwordController =
+      TextEditingController(text: '');
+
+  late ConfettiController _controllerBottomCenter;
+
+  @override
+  void initState() {
+    super.initState();
+    _controllerBottomCenter =
+        ConfettiController(duration: const Duration(seconds: 10));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Background(
+      child: Container(
+        padding: const EdgeInsets.symmetric(horizontal: 30),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const SizedBox(
+              height: 50,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Image.asset(MyIcons.keys),
+                    Text(
+                      "Level ${widget.level}",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w700,
+                        color: Colors.white,
+                        fontSize: 24,
+                      ),
+                    ),
+                  ],
+                ),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 20),
+                  child: SizedBox(
+                    height: 50,
+                    child: TextField(
+                      inputFormatters: [
+                        LengthLimitingTextInputFormatter(10),
+                      ],
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        filled: true,
+                        fillColor: Colors.white,
+                        border: OutlineInputBorder(
+                          borderSide: const BorderSide(color: Colors.black),
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        // enabled: false,
+                      ),
+                      textCapitalization: TextCapitalization.characters,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 50, vertical: 20),
+                  child: SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.zero,
+                        primary: MyColors.tropicalBlue,
+                        shadowColor: Colors.transparent.withOpacity(0),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                      ),
+                      onPressed: () {
+                        onSubmit(
+                          context: context,
+                          confettiController: _controllerBottomCenter,
+                          level: widget.level,
+                          code: _passwordController.text.toString(),
+                        );
+                      },
+                      child: const Text(
+                        'Kiểm tra',
+                        style: TextStyle(
+                          color: Colors.black,
+                          fontWeight: FontWeight.w700,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Image.asset(MyIcons.lockedLock),
+                Container(
+                  alignment: Alignment.center,
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 15, vertical: 12),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(25),
+                    color: MyColors.cornflowerBlue22,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: const [
+                      Text(
+                        "password: ctf-gdsc-dtu",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: ConfettiWidget(
+                confettiController: _controllerBottomCenter,
+                blastDirection: -pi / 2,
+                emissionFrequency: 0.01,
+                numberOfParticles: 20,
+                maxBlastForce: 100,
+                minBlastForce: 80,
+                gravity: 0.3,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
